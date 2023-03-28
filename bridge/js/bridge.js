5e014307aa1c9d2ca581bff4aff865a9238107ee
Sharon Kim <sharonita610@gmail.com>
2023-03-28 16:58:54 +0900

    bridge

--- bridge/js/bridge.js
+++ bridge/js/bridge.js
@@ -1,134 +1,39 @@
 
+// 1. 2차원 배열에서 왼쪽과 오른 쪽 중 하나를 선택합니다. [0,0][1,0]
+// 2. 선택한 동그라미가 =
+// - 정답이라면 한 칸 앞으로 나갑니다.
+// - 오답이라면 처음으로 돌아갑니다.
+// 3. 계속해서 정답을 선택하여 앞으로 나아가 종착지에 도착하면 성공입니다.
+//
+// 오답을 입력했다면 처음으로 돌아가서 다시 시도합니다.
+// const let
 
-glasses = document.getElementsByClassName('glass');
-left = document.getElementsByClassName('left');
-right = document.getElementsByClassName('right');
-div_finish = document.getElementById('#char-finish');
-step = "C://Users//1BestCsharp//Downloads//steps.png";
-nostep = "C://Users//1BestCsharp//Downloads//nostep.png";
-list = [step, nostep];
+
+//  변수이름 선언하기
+
+glasses = document.getElementsByName('glass');
+left = document.getElementsByName('left');
+right = document.getElementsByName('right');
+go = "C:\\memory-game\\bridge\\bridge-img\\bridge-img-girl1.jpg";
+ungo = "C:\\memory-game\\bridge\\bridge-img\\bridge-img-fail.png";
+list = [go , ungo];
 imagesOrder = [[]];
-counter = 0;
+step = 0;
 won = true;
 index = 0;
 
 
-// create a function to enable buttons
-function enableButton()
-{
-  if(counter <= left.length - 1)
-  {
-    left[counter].disabled = false;
-    right[counter].disabled = false;
-  }
-}
-
-// create a function to populate imagesOrder with random images
-function getRandomImages()
-{
-  for(let i = 0; i < left.length; i++)
-  {
-    imagesOrder[i] = [];
-    // get random image
-    random_image = list[Math.floor(Math.random()*list.length)];
-
-    if(random_image == step)
-    {
-      imagesOrder[i][0] = random_image;
-      imagesOrder[i][1] = nostep;
-    }
-    else
-    {
-      imagesOrder[i][0] = random_image;
-      imagesOrder[i][1] = step;
-    }
-
-    console.log(imagesOrder[i][1]);
-    console.log(imagesOrder[i][0]);
-    console.log("-------------------");
+// 노란칸에 랜덤 이미지를 [ o , o ] 적용해주는 함수 생성
+function getRandomImages(){
+  for(let i = 0; i < right.length; i++){
 
   }
 }
 
 
-function stepInThis(pic)
-{
-  if(pic.classList.contains('left'))
-  {
-    if(pic.disabled == false)
-    {
-      if(imagesOrder[counter][0] == nostep)
-      {
-        won = false;
-      }
-      if(counter == imagesOrder.length-1 && won == true)
-      {
-        div_finish.setAttribute("style",
-          "background-image: url('"+imagesOrder[counter][0]+"');");
-        alert("You've Won");
-      }
-      else if(won == false)
-      {
-        alert("You've Lost");
-      }
-
-      pic.setAttribute("style",
-        "background-image: url('"+imagesOrder[counter][0]+"');");
-      pic.disabled = true;
-      right[counter].disabled = true;
-      counter++;
-      enableButton();
-    }
-
-  }
-  else if(pic.classList.contains('right'))
-  {
-    if(pic.disabled == false)
-    {
-      if(imagesOrder[counter][1] == nostep)
-      {
-        won = false;
-      }
-      if(counter == imagesOrder.length-1 && won == true)
-      {
-        div_finish.setAttribute("style",
-          "background-image: url('"+imagesOrder[counter][1]+"');");
-        alert("You've Won");
-      }
-      else if(won == false)
-      {
-        alert("You've Lost");
-      }
-
-      pic.setAttribute("style",
-        "background-image: url('"+imagesOrder[counter][1]+"');");
-      pic.disabled = true;
-      left[counter].disabled = true;
-      counter++;
-      enableButton();
-    }
-  }
-}
-
-
-// create a function to play again
-function playagain()
-{
-  won = true;
-  for(let i = 0; i < left.length; i++)
-  {
-    left[i].setAttribute("style","background-image: url();");
-    right[i].setAttribute("style","background-image: url();");
-    left[i].disabled = true;
-    right[i].disabled = true;
-  }
-
-  counter = 0;
-  enableButton();
-  getRandomImages();
-}
 
 
 
-getRandomImages();
-enableButton();
+
+
+
