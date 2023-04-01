0f8ad413e18c9d4f99669b76e7740a7942fa12a7
Sharon Kim <sharonita610@gmail.com>
2023-04-02 02:28:54 +0900

    index.html footer change

---
+++ src/bridge/js/bridge-info-btn.js
@@ -0,0 +1,10 @@
+
+
+const $buttonClick = document.getElementById('info-btn');
+const $gameInfo = document.getElementById()
+
+console.log(buttonClick);
+
+$buttonClick.addEventListener('click',() => {
+
+});
